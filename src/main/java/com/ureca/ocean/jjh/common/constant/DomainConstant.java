5d506ea2bb95383131a7ca58663953333cbc9e68
jmk445 <jmk445123456@gmail.com>
2025-07-15 11:38:00 +0900

    feat : common/config, constant 추가

---
+++ src/main/java/com/ureca/ocean/jjh/common/constant/DomainConstant.java
@@ -0,0 +1,7 @@
+package com.ureca.ocean.jjh.common.constant;
+
+
+public class DomainConstant {
+//    public static final String USER_URL = "http://localhost:8082/";
+    public static final String USER_URL = "http://10.0.2.41:8082/";
+}
