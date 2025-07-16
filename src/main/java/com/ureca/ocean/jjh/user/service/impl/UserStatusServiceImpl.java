0f1dd07127cc2b2d21f8e16a6facd4b8ed044845
jmk445 <jmk445123456@gmail.com>
2025-07-16 11:45:54 +0900

    fix : put 을 잘 못된 방향ì법으로 하고 이썼던 것 해결

--- src/main/java/com/ureca/ocean/jjh/user/service/impl/UserStatusServiceImpl.java
+++ src/main/java/com/ureca/ocean/jjh/user/service/impl/UserStatusServiceImpl.java
@@ -8,11 +8,14 @@ import com.ureca.ocean.jjh.user.entity.UserStatus;
 import com.ureca.ocean.jjh.user.repository.UserRepository;
 import com.ureca.ocean.jjh.user.repository.UserStatusRepository;
 import com.ureca.ocean.jjh.user.service.UserStatusService;
+
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class UserStatusServiceImpl implements UserStatusService {
     private final UserStatusRepository userStatusRepository;
     private final UserRepository userRepository;
@@ -37,12 +40,15 @@ public class UserStatusServiceImpl implements UserStatusService {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(()->new UserException(ErrorCode.NOT_FOUND_USER));
         UserStatus userStatusFound = userStatusRepository.findById(user.getId()).orElseThrow(()->new UserException(ErrorCode.USER_STATUS_NOT_EXIST));
-        UserStatus newUserStatus = UserStatus.builder()
-                .user(user)
-                .level(userStatusFound.getLevel()+levelChange)
-                .exp(userStatusFound.getExp() + expChange)
-                .build();
-        UserStatus userStatus = userStatusRepository.save(newUserStatus);
+        userStatusFound.setLevel(userStatusFound.getLevel()+levelChange);
+        userStatusFound.setExp(userStatusFound.getExp() + expChange);
+
+//        UserStatus newUserStatus = UserStatus.builder()
+//                .user(user)
+//                .level(userStatusFound.getLevel()+levelChange)
+//                .exp(userStatusFound.getExp() + expChange)
+//                .build();
+        UserStatus userStatus = userStatusRepository.save(userStatusFound);
 
         return UserStatusResponseDto.builder()
                 .userId(userStatus.getId())
