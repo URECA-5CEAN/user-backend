c5f8b624a14c24a962f829447e00bef023f5084e
jmk445 <jmk445123456@gmail.com>
2025-07-17 22:22:00 +0900

    feat : swagger에서 token 값도 넣을 수 있도록

--- src/main/java/com/ureca/ocean/jjh/config/SwaggerConfig.java
+++ src/main/java/com/ureca/ocean/jjh/config/SwaggerConfig.java
@@ -1,16 +1,30 @@
 package com.ureca.ocean.jjh.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(title = "User API", version = "v1"),
-    servers = {
-        @Server(url = "http://15.164.81.45", description = "External server")
-    }
+        info = @Info(title = "User API", version = "v1"),
+        servers = {
+                @Server(url = "http://15.164.81.45", description = "External server")
+        },
+        security = {
+                @SecurityRequirement(name = "bearerAuth")  // 👈 아래 SecurityScheme과 연결
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",                           // 👈 Swagger UI에서 선택할 이름
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class SwaggerConfig {
 }
