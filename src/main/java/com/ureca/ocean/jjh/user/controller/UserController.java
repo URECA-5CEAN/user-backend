1bb0261fa4b5231af34f7ac8099d5a8174b3db5b
jmk445 <jmk445123456@gmail.com>
2025-07-17 22:33:25 +0900

    fix : RequestHeader를 swagger에서 안보이게

--- src/main/java/com/ureca/ocean/jjh/user/controller/UserController.java
+++ src/main/java/com/ureca/ocean/jjh/user/controller/UserController.java
@@ -12,6 +12,7 @@ import com.ureca.ocean.jjh.user.dto.response.UserStatusResponseDto;
 import com.ureca.ocean.jjh.user.service.impl.AttendanceServiceImpl;
 import com.ureca.ocean.jjh.user.service.impl.UserServiceImpl;
 import com.ureca.ocean.jjh.user.service.impl.UserStatusServiceImpl;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -25,20 +26,21 @@ import java.nio.charset.StandardCharsets;
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
+
     private final UserServiceImpl userServiceImpl;
     private final UserStatusServiceImpl userStatusServiceImpl;
     private final AttendanceServiceImpl attendanceServiceImpl;
+
     @PostMapping("/health")
-    public String health(){
+    public String health() {
         log.info("user health checking...");
         return "user health check fine";
     }
 
     @GetMapping
-    public ResponseEntity<BaseResponseDto<?>> getUserByEmail(@RequestParam String email){
+    public ResponseEntity<BaseResponseDto<?>> getUserByEmail(@RequestParam String email) {
         UserResponseDto userDto = userServiceImpl.getUserByEmail(email);
-        //email 기준으로 해당 사용자가 없을 경우
-        if(userDto.getId() == null){
+        if (userDto.getId() == null) {
             return ResponseEntity.badRequest().body(BaseResponseDto.fail(ErrorCode.NOT_FOUND_USER));
         }
         log.info(userDto.getPassword());
@@ -46,48 +48,52 @@ public class UserController {
     }
 
     @GetMapping("/isDupNickname")
-    public ResponseEntity<BaseResponseDto<?>> getIsDupNickname(@RequestParam String nickname){
+    public ResponseEntity<BaseResponseDto<?>> getIsDupNickname(@RequestParam String nickname) {
         return ResponseEntity.ok(BaseResponseDto.success(userServiceImpl.getIsDupNickname(nickname)));
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<BaseResponseDto<?>>  signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
+    public ResponseEntity<BaseResponseDto<?>> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         UserResponseDto userDto = userServiceImpl.signUp(signUpRequestDto);
         return ResponseEntity.ok(BaseResponseDto.success(userDto));
     }
 
     @PostMapping("/currentUserInfo")
-    public ResponseEntity<BaseResponseDto<?>>  getCurrentUserInfo(@RequestHeader("X-User-email") String encodedEmail) {
+    public ResponseEntity<BaseResponseDto<?>> getCurrentUserInfo(
+            @Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail) {
         String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
-        log.info("user-backend내의 currnet userEmail : " + email);
+        log.info("user-backend 내의 current userEmail : " + email);
         UserResponseDto userDto = userServiceImpl.getCurrentUserInfo(email);
-        if(userDto.getId() == null){
+        if (userDto.getId() == null) {
             return ResponseEntity.badRequest().body(BaseResponseDto.fail(ErrorCode.NOT_FOUND_USER));
         }
         log.info(userDto.getPassword());
         return ResponseEntity.ok(BaseResponseDto.success(userDto));
     }
 
-
     @GetMapping("/stat")
-    public ResponseEntity<BaseResponseDto<?>>  getUserStatus(@RequestHeader("X-User-email") String encodedEmail) {
+    public ResponseEntity<BaseResponseDto<?>> getUserStatus(
+            @Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail) {
         String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
         log.info("user-backend 내의 current userEmail : " + email);
         UserStatusResponseDto userStatusResponseDto = userStatusServiceImpl.getUserStatus(email);
         return ResponseEntity.ok(BaseResponseDto.success(userStatusResponseDto));
     }
 
-
     @PutMapping("/stat")
-    public ResponseEntity<BaseResponseDto<?>>  modifyUserStatus(@RequestHeader("X-User-email") String encodedEmail, @RequestBody UserStatusRequestDto userStatusRequestDto) {
+    public ResponseEntity<BaseResponseDto<?>> modifyUserStatus(
+            @Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail,
+            @RequestBody UserStatusRequestDto userStatusRequestDto) {
         String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
         log.info("user-backend 내의 current userEmail : " + email);
-        UserStatusResponseDto userStatusResponseDto = userStatusServiceImpl.changeUserStatus(email,userStatusRequestDto.getLevelChange(),userStatusRequestDto.getExpChange());
+        UserStatusResponseDto userStatusResponseDto = userStatusServiceImpl.changeUserStatus(
+                email, userStatusRequestDto.getLevelChange(), userStatusRequestDto.getExpChange());
         return ResponseEntity.ok(BaseResponseDto.success(userStatusResponseDto));
     }
 
     @PostMapping("/attendance")
-    public ResponseEntity<BaseResponseDto<?>>  insertAttendance(@RequestHeader("X-User-email") String encodedEmail) {
+    public ResponseEntity<BaseResponseDto<?>> insertAttendance(
+            @Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail) {
         String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
         log.info("user-backend 내의 current userEmail : " + email);
         AttendanceResponseDto userStatusResponseDto = attendanceServiceImpl.insertAttendance(email);
@@ -95,11 +101,13 @@ public class UserController {
     }
 
     @GetMapping("/attendance")
-    public ResponseEntity<BaseResponseDto<?>> listAttendance(@RequestHeader("X-User-email") String encodedEmail, @RequestParam Integer year, @RequestParam Integer month) {
+    public ResponseEntity<BaseResponseDto<?>> listAttendance(
+            @Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail,
+            @RequestParam Integer year,
+            @RequestParam Integer month) {
         String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
         log.info("user-backend 내의 current userEmail : " + email);
-        AttendanceListResponseDto attendanceListResponseDto = attendanceServiceImpl.listAttendance(email,year,month);
+        AttendanceListResponseDto attendanceListResponseDto = attendanceServiceImpl.listAttendance(email, year, month);
         return ResponseEntity.ok(BaseResponseDto.success(attendanceListResponseDto));
     }
-
 }
