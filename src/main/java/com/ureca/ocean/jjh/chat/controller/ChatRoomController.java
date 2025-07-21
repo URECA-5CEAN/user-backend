d9fb415b6b00678e9476e056fa0e24727684cb52
jmk445 <jmk445123456@gmail.com>
2025-07-21 18:31:37 +0900

    fix : email decode

--- src/main/java/com/ureca/ocean/jjh/chat/controller/ChatRoomController.java
+++ src/main/java/com/ureca/ocean/jjh/chat/controller/ChatRoomController.java
@@ -6,18 +6,25 @@ import com.ureca.ocean.jjh.chat.service.impl.ChatRoomServiceImpl;
 import com.ureca.ocean.jjh.common.BaseResponseDto;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/user/chatRoom")
 @RequiredArgsConstructor
+@Slf4j
 public class ChatRoomController {
     private final ChatRoomServiceImpl chatRoomService;
     @PostMapping
     public ResponseEntity<BaseResponseDto<?>> insertChatRoom(@Parameter(hidden = true) @RequestHeader("X-User-email") String encodedEmail,
                                                              @RequestBody ChatRoomRequestDto chatRoomRequestDto){
-        ChatRoomResponseDto ChatRoomResponseDto = chatRoomService.insertChatRoom(encodedEmail, chatRoomRequestDto.getPostId());
+        String email = URLDecoder.decode(encodedEmail, StandardCharsets.UTF_8);
+        log.info("user-backend 내의 current userEmail : " + email);
+        ChatRoomResponseDto ChatRoomResponseDto = chatRoomService.insertChatRoom(email, chatRoomRequestDto.getPostId());
         return ResponseEntity.ok(BaseResponseDto.success(ChatRoomResponseDto));
     }
 }
