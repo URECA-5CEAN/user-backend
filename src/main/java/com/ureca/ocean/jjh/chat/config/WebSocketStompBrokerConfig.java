9b43c5130bd5146a9e86242e5caa0cb296cb4c8f
jmk445 <jmk445123456@gmail.com>
2025-07-22 17:27:13 +0900

    feat : setAllowedOriginPatterns 로 변경

--- src/main/java/com/ureca/ocean/jjh/chat/config/WebSocketStompBrokerConfig.java
+++ src/main/java/com/ureca/ocean/jjh/chat/config/WebSocketStompBrokerConfig.java
@@ -20,11 +20,12 @@ public class WebSocketStompBrokerConfig implements WebSocketMessageBrokerConfigu
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트가 연결할 엔드포인트 (SockJS fallback 포함)
         registry.addEndpoint("/api/user/chat/ws")
-                .setAllowedOrigins(
-                        "http://127.0.0.1:5500",
-                        "http://localhost:5500",
-                        "http://15.164.81.45"
-                )
+//                .setAllowedOrigins(
+//                        "http://127.0.0.1:5500",
+//                        "http://localhost:5500",
+//                        "http://15.164.81.45"
+//                )
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
