de0ce294e679c09d72810e3dda16c39830a3eed3
Jisan <kjs25481@gmail.com>
2025-07-23 15:32:14 +0900

    feat : ChatRoomUserRepository 의 ID 수정

--- src/main/java/com/ureca/ocean/jjh/chat/repository/ChatRoomUserRepository.java
+++ src/main/java/com/ureca/ocean/jjh/chat/repository/ChatRoomUserRepository.java
@@ -1,11 +1,13 @@
 package com.ureca.ocean.jjh.chat.repository;
 
 import com.ureca.ocean.jjh.chat.entity.ChatRoomUser;
+import com.ureca.ocean.jjh.chat.entity.id.ChatRoomUserId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ChatRoomUserRepository  extends JpaRepository<ChatRoomUser, UUID> {
+public interface ChatRoomUserRepository  extends JpaRepository<ChatRoomUser, ChatRoomUserId> {
 }
